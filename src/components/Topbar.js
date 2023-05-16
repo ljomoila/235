8dd72c0336bf328a88145496d9c0a4a0a9604767
ljomoila <joonas.finncodex@gmail.com>
2023-05-16 16:41:13 +0200

    style: clean up

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,20 +1,20 @@
 import { Button, Text, View } from 'react-native';
 
 export const Topbar = ({ left, title, right }) => {
-  return (
-    <View
-      style={{
-        display: 'flex',
-        width: '100%',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 5
-      }}
-    >
-      {left ? <Button title={left.title} onPress={() => left.onPress()} /> : <View></View>}
-      {title ? <Text style={{ color: 'white' }}>{title}</Text> : <View></View>}
-      {right ? <Button title={right.title} onPress={() => right.onPress()} /> : <View></View>}
-    </View>
-  );
+    return (
+        <View
+            style={{
+                display: 'flex',
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                marginBottom: 5
+            }}
+        >
+            {left ? <Button title={left.title} onPress={() => left.onPress()} /> : <View></View>}
+            {title ? <Text style={{ color: 'white' }}>{title}</Text> : <View></View>}
+            {right ? <Button title={right.title} onPress={() => right.onPress()} /> : <View></View>}
+        </View>
+    );
 };
--- src/service/Api.js
+++ src/service/Api.js
@@ -1,54 +1,54 @@
 const NHL_API_BASE_URL = 'https://statsapi.web.nhl.com/api/v1';
 
 export default class Api {
-  async doFetch(url) {
-    try {
-      const response = await fetch(NHL_API_BASE_URL + url);
-      return response.json();
-    } catch (e) {
-      throw new Error(`Fetch failed ${e}`);
+    async doFetch(url) {
+        try {
+            const response = await fetch(NHL_API_BASE_URL + url);
+            return response.json();
+        } catch (e) {
+            throw new Error(`Fetch failed ${e.message}`);
+        }
     }
-  }
 
-  async getTeams() {
-    const response = await this.doFetch('/teams');
-    return response.teams;
-  }
+    async getTeams() {
+        const response = await this.doFetch('/teams');
+        return response.teams;
+    }
 
-  async getTeam(id) {
-    const response = await this.doFetch('/teams/' + id);
-    return response.teams;
-  }
+    async getTeam(id) {
+        const response = await this.doFetch('/teams/' + id);
+        return response.teams;
+    }
 
-  async getRoster(teamId) {
-    const response = await this.doFetch('/teams/' + teamId + '/roster');
-    return response.roster;
-  }
+    async getRoster(teamId) {
+        const response = await this.doFetch(`/teams/${teamId}/roster`);
+        return response.roster;
+    }
 
-  async getPlayer(playerId) {
-    const response = await this.doFetch('/people/' + playerId);
-    return response.people[0];
-  }
+    async getPlayer(playerId) {
+        const response = await this.doFetch('/people/' + playerId);
+        return response.people[0];
+    }
 
-  async getSchedule() {
-    const response = await this.doFetch('/schedule');
-    return response.dates.length > 0 ? response.dates[0].games : [];
-  }
+    async getSchedule() {
+        const response = await this.doFetch('/schedule');
+        return response.dates.length > 0 ? response.dates[0].games : [];
+    }
 
-  async getScheduleByDate(date) {
-    const response = await this.doFetch(`/schedule?date=${date}`);
+    async getScheduleByDate(date) {
+        const response = await this.doFetch(`/schedule?date=${date}`);
 
-    return response.dates.length ? response.dates[0].games : [];
-  }
+        return response.dates.length ? response.dates[0].games : [];
+    }
 
-  async getLiveFeed(gamePk) {
-    return await this.doFetch(`/game/${gamePk}/feed/live`);
-  }
+    async getLiveFeed(gamePk) {
+        return await this.doFetch(`/game/${gamePk}/feed/live`);
+    }
 
-  async getPlayerStats(playerId, statType) {
-    const response = await this.doFetch(`/people/${playerId}/stats?stats=${statType}`);
-    if (response.stats.length) return response.stats[0].splits;
+    async getPlayerStats(playerId, statType) {
+        const response = await this.doFetch(`/people/${playerId}/stats?stats=${statType}`);
+        if (response.stats.length) return response.stats[0].splits;
 
-    return [];
-  }
+        return [];
+    }
 }
