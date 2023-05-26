e96e7bf51ce77b1e01cba8da207b21bed29617ef
ljomoila <joonas.finncodex@gmail.com>
2023-05-26 16:55:11 +0200

    style: clean up

--- src/components/ScoresTopbar.js
+++ src/components/ScoresTopbar.js
@@ -3,29 +3,31 @@ import { AppContext } from '../context/App/AppContext';
 import { Topbar } from './Topbar';
 
 export const getDateStr = (index) => {
-  let date = new Date();
-  date.setDate(date.getDate() + (index || 0));
+    let date = new Date();
+    date.setDate(date.getDate() + (index || 0));
 
-  return formatDate(date);
+    console.log(formatDate(date));
+
+    return formatDate(date);
 };
 
 const formatDate = (date) => {
-  return date.toISOString().split('T')[0];
+    return date.toISOString().split('T')[0];
 };
 
 export const ScoresTopbar = () => {
-  const { appState, dispatch } = useContext(AppContext);
+    const { appState, dispatch } = useContext(AppContext);
 
-  const onButtonPress = (i) => {
-    const newDateIndex = appState.dateIndex + i;
-    dispatch({ ...appState, dateIndex: newDateIndex, updateScores: true });
-  };
+    const onButtonPress = (i) => {
+        const newDateIndex = appState.dateIndex + i;
+        dispatch({ ...appState, dateIndex: newDateIndex, updateScores: true });
+    };
 
-  return (
-    <Topbar
-      title={getDateStr(appState.dateIndex)}
-      left={{ title: 'Back', onPress: () => onButtonPress(-1) }}
-      right={{ title: 'Forward', onPress: () => onButtonPress(1) }}
-    />
-  );
+    return (
+        <Topbar
+            title={getDateStr(appState.dateIndex)}
+            left={{ title: 'Back', onPress: () => onButtonPress(-1) }}
+            right={{ title: 'Forward', onPress: () => onButtonPress(1) }}
+        />
+    );
 };
