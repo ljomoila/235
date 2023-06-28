7828857811bce1c0f1f880f76b5cf25385c0ee1b
ljomoila <joonas.finncodex@gmail.com>
2023-06-28 19:35:51 +0200

    doc: commenting

--- src/components/SwipeView.js
+++ src/components/SwipeView.js
@@ -7,17 +7,18 @@ const SwipeView = ({ style, children }) => {
     const { appState, dispatch } = useContext(AppContext);
     const { onTouchStart, onTouchEnd } = useSwipe(onSwipeLeft, onSwipeRight, 6);
 
-    function onSwipeLeft() {
+    // TODO: put views in list when more than two views
+    const onSwipeLeft = () => {
         if (appState.activeView === Views.SELECT_COUNTRY) {
             dispatch({ ...appState, activeView: Views.SCORES });
         }
-    }
+    };
 
-    function onSwipeRight() {
+    const onSwipeRight = () => {
         if (appState.activeView === Views.SCORES) {
             dispatch({ ...appState, activeView: Views.SELECT_COUNTRY });
         }
-    }
+    };
 
     return (
         <ScrollView style={style} onTouchStart={onTouchStart} onTouchEnd={onTouchEnd}>
